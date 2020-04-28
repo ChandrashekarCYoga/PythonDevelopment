3063c55f188be499d49f603b2bf07ff3f160d2e0
Chandrashekar C <chandrashekar.c@gmail.com>
2020-04-28 08:39:26 +0000

    Datatypes, Variables and Operator Precedence files added

---
+++ DataTypes.py
@@ -0,0 +1,28 @@
+# Fundatamental Data Types
+
+# int and float
+# Complex -> for complex math(real, imagine)
+
+print(type(6))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+print(type(10.56))
+print(type(20+1.1))
+
+print(type(9.9+1.1))
+
+# power of
+print(2 ** 3) 
+
+print(5 // 4)
+
+# Remainder
+print(5 % 4)
+
+# Math functions
+print(round(3.1))
+print(round(3.9))
+
+print(abs(-20))
---
+++ Variables.py
@@ -0,0 +1,35 @@
+# store information in program
+
+# snake_case
+# start with lowercase or underscore
+# _variables are private _variables
+# variables are case sensitive
+# don't overwrite keywords
+# descriptive
+
+user_iq = 190
+
+user_age = user_iq/4
+
+print(user_iq)
+print(user_age)
+
+
+# constants
+# value don't change
+# don't create value with two underscore, not good practice
+
+PI = 3.14
+
+print(PI)
+
+a, b, c = 1, 2, 3
+
+print(a)
+
+print(b)
+
+print(c)
+
+
+
