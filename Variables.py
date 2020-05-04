3105c29dea13234255589e03c1a45c1257c5dd16
Chandrashekar C <chandrashekar.c@gmail.com>
2020-05-04 20:11:27 +0000

    variables, types, strings operations, list, tuples, dit

--- Variables.py
+++ Variables.py
@@ -32,4 +32,130 @@ print(b)
 print(c)
 
 
+# augmented assignment operator
+some_value = 5
+some_value += 2
+print(type(some_value))
 
+print(some_value)
+
+x = "Hello World"
+print(x)
+print(type(x))
+
+long_string ='''
+WOW
+O O
+---
+'''
+print(long_string)
+print(type(long_string))
+
+x = 20
+print(x)
+print(type(x))
+
+x = 20.5
+print(x)
+print(type(x))
+
+x = 1j
+print(x)
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x))
+
+x = range(6)
+print(x)
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+x = True
+print(x)
+print(type(x))
+
+x = b"Hello"
+print(x)
+print(type(x))
+
+x = bytearray(5)
+print(x)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
+
+# Escape sequence
+weather = "It\'s \"kind of\" sunny"
+print(weather)
+
+weather = "\tIt\'s \"kind of\" sunny"
+print(weather)
+
+name = 'Johnny'
+age = 55
+
+print(f'Hi {name}, You are {age} years old')
+
+print('Hi {}. You are {} years old'.format('Johnny', '55'))
+
+print('Hi {}. You are {} years old'.format(name, age))
+
+print('Hi {0}. You are {1} years old'.format(name, age))
+
+print('Hi {new_name}. You are {age} years old'.format(new_name='sally', age=100))
+
+
+selfish = 'me me me'
+        #  01234567
+
+print(selfish[0])
+
+selfish = '01234567'
+# [start:stop:stepover]
+# string slicing
+print(selfish[4:7:2])
+print(selfish[1:])
+print(selfish[:5])
+
+print(selfish[::1])
+
+print(selfish[-1])
+print(selfish[-2])
+print(selfish[-3])
+
+# reverse
+print(selfish[::-1])
+print(selfish[::-2])
+
+# strings are immutable in python
+
+quote = 'to be or not to be'
+print(quote.upper())
+print(quote.upper().lower())
+
+print(quote.capitalize())
+
+print(quote.find('be'))
+
+print(quote.replace('be', 'me'))
+
+print(quote)
